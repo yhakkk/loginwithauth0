e5e26b27a26dea609f05987545d407a87bc5675e
yhakkk <yhakblanco@gmail.com>
2024-12-16 10:40:45 -0300

    featfd

--- src/pages/Login/LoginPage.js
+++ src/pages/Login/LoginPage.js
@@ -2,9 +2,16 @@ import LoginButton from "../../components/login"
 
 const LoginPage = () =>{
 
+    const handleLogin=()=>{
+        console.log('login')
+    }
+    
     return(
         <>
         <LoginButton />
+        <h1>hola</h1>
+        <h1>hola</h1>
+        <h1>hola</h1>
         <h1>hola</h1>
         </>
     )
